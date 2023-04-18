3a3ae283054b20d2a9597c904dbcf2a6246908cd
Othmane EL AHMADI <othmaneelahmadis@gmail.com>
2023-04-18 02:14:36 +0000

    fix

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   // Validate form data
   if (empty($nom) || empty($prenom) || empty($email) || empty($tel) || empty($code)) {
-    $error = "All fields are required";
+    $error = "Tous les champs sont obligatoires.";
     $oldNom = $nom;
     $oldPrenom = $prenom;
     $oldEmail = $email;
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $sql = "INSERT INTO client (nom, prenom, email, tel, code) VALUES ('$nom', '$prenom', '$email', '$tel', '$code')";
 
     if ($conn->query($sql) === TRUE) {
-      $success = "Client added successfully";
+      $success = "Client ajouté avec succès";
       $oldNom = "";
       $oldPrenom = "";
       $oldEmail = "";
@@ -73,7 +73,7 @@ else{
     <meta name="author" content="ZXing for JS">
 
     <title>ASWAK SOUSS | Carte de fidélité</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
 
@@ -96,80 +96,80 @@ else{
 </div>
    
     <main class="container my-5">
-        <section id="client-info row">
-            <form action="index.php" method="post" id="client_data">
-                <div class="form-group row">
-                    <label for="nom" class="col-sm-2 col-form-label col-form-label-sm">1 introduire nom</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control form-control-sm w-75 mx-auto" id="nom" name="nom" value="<?php echo htmlspecialchars($oldNom); ?>" placeholder="Nom" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="prenom" class="col-sm-2 col-form-label col-form-label-sm">2 introduire prénom</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control form-control-sm w-75 mx-auto" id="prenom" name="prenom" value="<?php echo htmlspecialchars($oldPrenom); ?>" placeholder="Prénom" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="tel" class="col-sm-2 col-form-label col-form-label-sm">3 introduire tel</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control form-control-sm w-75 mx-auto" id="prenom" name="tel" value="<?php echo htmlspecialchars($oldTel); ?>" placeholder="Tel" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="email" class="col-sm-2 col-form-label col-form-label-sm">4 introduire email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control form-control-sm w-75 mx-auto" id="email" name="email" value="<?php echo htmlspecialchars($oldEmail); ?>" placeholder="Email" required>
-                    </div>
-                </div>
-                <div class="form-group row">
-                    <label for="codeCarte" class="col-sm-2 col-form-label col-form-label-sm">6 scanner une carte de fidélité</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control form-control-sm w-75 mx-auto" id="result" name="code" value="<?php echo htmlspecialchars($oldCode); ?>" placeholder="code barre" required>
-                    </div>
-                </div>
-            </form>
-
-            <div id="save" class="col-md-12 ">
-                    <div class="w-100">
-                        <input type="submit" value="Ajouter" id="post" class="save-btn btn btn-success text-white my-3 rounded">
-                    </div>
-            </div>
-
+        <div id="client-info row">
             <div class="col-md-12">
-                <div class="reader w-50 ml-auto">
-                    <div>
-                        <video id="video" width="100%" height="40%"></video>
+                <form action="index.php" method="post" id="client_data">
+                    <div class="form-group ">
+                        <div >
+                            <input type="text" class="form-control form-control-sm " id="nom" name="nom" value="<?php echo htmlspecialchars($oldNom); ?>" placeholder="Nom" required>
+                        </div>
                     </div>
-                    <!-- MULTI DEVICES
-                    <div id="sourceSelectPanel" style="display:none" class="my-2">
-                        <label for="sourceSelect">Change video source:</label>
-                        <select id="sourceSelect" class="custom-select">
-                        </select>
+                    <div class="form-group ">
+                        <div >
+                            <input type="text" class="form-control form-control-sm " id="prenom" name="prenom" value="<?php echo htmlspecialchars($oldPrenom); ?>" placeholder="Prénom" required>
+                        </div>
                     </div>
-                     -->
-                    <div class="my-3 px-3">
-                        <button id="startButton" class="btn btn-success mx-2">Start</button>
-                        <button id="resetButton" class="btn btn-warning mx-2">Reset</button>
+                    <div class="form-group ">
+                        <div>
+                            <input type="text" class="form-control form-control-sm " id="prenom" name="tel" value="<?php echo htmlspecialchars($oldTel); ?>" placeholder="Tel" required>
+                        </div>
                     </div>
-                   
-
-                </div>
+                    <div class="form-group ">
+                        <div>
+                            <input type="email" class="form-control form-control-sm " id="email" name="email" value="<?php echo htmlspecialchars($oldEmail); ?>" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <div class="form-group ">
+                        <div>
+                            <input type="text" class="form-control form-control-sm " id="result" name="code" value="<?php echo htmlspecialchars($oldCode); ?>" placeholder="code barre" required>
+                        </div>
+                    </div>
+                </form>
             </div>
-
-        </section>
+            </div>
+            <div class="container row">
+            <div class="col-md-6">
+                    <input type="submit" value="Enregistrer" id="post" class="save-btn btn btn-success text-white my-3 rounded w-75">
+            </div>
+            <div class="col-md-6">
+                        <div class="reader my-3">
+                            <div>
+                                <video id="video" width="100%" height="40%" title="Barre code Scanner v1"></video>
+                            </div>
+                            <div class="my-3">
+                                <button id="startButton" class="btn btn-success mx-2 w-40">Start scanner</button>
+                                <button id="resetButton" class="btn btn-warning mx-2 w-40">Reset scanner</button>
+                            </div>
+                           
+                        </div>
+            </div>
+            </div>
     </main>
-    <div class="description w-90 mx-5">
-        <p>Nous avons des carte de fidélité de numéro 1 à 10000<br> pour chaque client on lui demande ses infos (nom prenom etc)<br> on prend une carte on la scane et on attribue son numero au client<br> et on engregistre le tout dans une base de donnée
-        </p>
+    <div class="modal fade" id="barcodeModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">Scanner</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <!-- Barcode data will be inserted here -->
+        </div>
+        </div>
     </div>
+    </div>
+
+ 
 
     <audio id="validation-sound">
 		<source src="/sounds/beep.wav" type="audio/mp3" hidden>
 	</audio>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+
+  
     <script type="text/javascript" src="/js/scaner.min.js"></script>
     <script type="text/javascript">
         window.addEventListener('load', function() {
@@ -178,27 +178,7 @@ else{
             console.log('ZXing code reader initialized')
             codeReader.listVideoInputDevices()
                 .then((videoInputDevices) => {
-                    /* MULTI DEVICES
-                    const sourceSelect = document.getElementById('sourceSelect')
-                    console.log(videoInputDevices);
-                    selectedDeviceId = videoInputDevices[0].deviceId
-                    if (videoInputDevices.length >= 1) {
-                        videoInputDevices.forEach((element) => {
-                            const sourceOption = document.createElement('option')
-                            sourceOption.text = element.label
-                            sourceOption.value = element.deviceId
-                            sourceSelect.appendChild(sourceOption)
-                        })
-
-                        sourceSelect.onchange = () => {
-                            selectedDeviceId = sourceSelect.value;
-                        };
-
-                        const sourceSelectPanel = document.getElementById('sourceSelectPanel')
-                        sourceSelectPanel.style.display = 'block'
-                    }
-                    */
-
+                   
                     document.getElementById('startButton').addEventListener('click', () => {
                         codeReader.decodeFromVideoDevice(undefined, 'video', (result, err) => {
                             if (result) {
@@ -220,11 +200,13 @@ else{
 
                                         // Check if the code exists in the client table
                                         if (response.exists) {
-                                            alert("Code exists in client table");
+                                            $('#barcodeModal .modal-body').text("Le code a déjà été attribué à un autre client.");
+                                            $('#barcodeModal').modal('show');
                                         } else {
                                             document.getElementById('result').value = "";
                                             document.getElementById('result').value = result.text
-                                            alert("Scanned : " + result.text)
+                                            $('#barcodeModal .modal-body').text("Sanned : "+result.text);
+                                            $('#barcodeModal').modal('show');
                                         }
                                         }
                                     };
